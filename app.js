c482c8f6b9f4845c5b1477824213c247cd03ead6
Joao Vitor Costa <139905189+JoaoVitorCRamos@users.noreply.github.com>
2023-09-15 03:05:09 -0300

    botei um pouco de css e fiz mais rotas

--- app.js
+++ app.js
@@ -1,29 +1,46 @@
 const express = require("express");
 const app = express();
 const articlesData = require("./data/articles.json");
-const userData = require("./data/users.json")
+const bodyParser = require("body-parser"); 
+const authenticateUser = require("./middwares/authenticator");
+const userData = require("./data/users.json");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
 
 app.get("/", function(req, res) {
-    res.sendFile(__dirname + '/views/index.html');
+    res.render(__dirname + '/views/index.ejs', {articles : articlesData});
 });
 
 //editar usuario
-app.get("/useredit/", function(req, res) {
-    res.sendFile(__dirname + '/views/users_edit.html');
+app.get("/useredit", function(req, res) {
+    res.render(__dirname + '/views/users_edit.ejs');
 });
 
 //Cadastrar usuario
 app.get("/usercreate", function(req, res) {
-    res.sendFile(__dirname + '/views/users_create.html');
+    res.render(__dirname + '/views/users_create.ejs');
 });
 
 //pagina administracao
-app.get("/administracao", function(req, res) {
-    res.sendFile(__dirname + '/views/admin.html');
+app.get("/administracao/:userId", function(req, res) {
+    const userId = req.params.userId;
+    const user = userData.find((userData) => userData.author_id === userId);
+
+    //checar se estao tentando passar a perna nos adms
+
+    if (user && user.author_level === "admin") {
+        res.render(__dirname + '/views/admin.ejs', {articles : articlesData});
+    } else {
+        res.status(403).send("Acesso negado. Você não é um administrador. :c");
+    }
 });
 
 //pagina de login
-app.get("/login", function (req, res) {
+app.route("/login").get(function (req, res) {
 
     res.send(`
     <!DOCTYPE html>
@@ -31,25 +48,28 @@ app.get("/login", function (req, res) {
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="/style/login.css">
             <title>Document</title>
         </head>
         <body>
             <div>
-                <h1>Login</h1>
-                <a href="http://localhost:8081/">Home</a>
+                <div>
+                    <h1>Login</h1>
+                    <a href="http://localhost:8081/">Home</a>
+                </div>
+                <form action="/login" method="POST">
+                    <label for="login">Usuário:</label>
+                    <input type="text" id="login" name="login"><br>
+                    <label for="password">Senha:</label>
+                    <input type="password" id="password" name="password"><br>
+                    <input type="submit" value="login">
+                </form>
             </div>
-            <form action="/login" method="POST">
-                <label for="login">Usuário:</label>
-                <input type="text" id="login" name="login"><br>
-                <label for="password">Senha:</label>
-                <input type="password" id="password" name="password"><br>
-                <input type="submit" value="login">
-            </form>
         </body>
     </html>
     `);
 
-});
+}).post(authenticateUser);
 
 //pagina de artigos
 app.get("/article/:permalink", function (req, res) {
@@ -57,27 +77,15 @@ app.get("/article/:permalink", function (req, res) {
 
     // Procurar o artigo pelo permalink no array de artigos
     const article = articlesData.find((a) => a.kb_permalink === permalink);
+    const user = userData.find((u) => u.author_email === article.kb_author_email);
 
     if (article) {
-        // Renderiza a página com título, texto e botão "like"
-        res.send(`
-            <!DOCTYPE html>
-            <html>
-                <body>
-                    <div>
-                        <a href="http://localhost:8081/">Home</a>
-                    </div>
-                    <h1>${article.kb_title}</h1>
-                    <p>${article.kb_body}</p>
-                    <button id="likeButton">Like</button>
-                </body>
-            </html>
-        `);
+        res.render(__dirname + '/views/articles_read.ejs', {article : article, user : user});
     } else {
         res.status(404).send("Artigo não encontrado :c");
     }
 });
 
 app.listen(8081, function(){
-    console.log("servidor rodando suavao");
+    console.log("servidor rodando suavao em http://localhost:8081/");
 });
---
+++ middwares/authenticator.js
@@ -0,0 +1,26 @@
+const usersData = require("../data/users.json");
+
+function authenticateUser(req, res, next) {
+  const { login, password } = req.body;
+
+  // achar o usuario
+  const user = usersData.find((userData) => userData.author_user === login);
+
+  if (!user || user.author_pwd !== password) {
+    // verificar se a senha esta errada
+    res.redirect("http://localhost:8081");
+    return;
+  }
+
+  // Checar se é admin ou usuario comum
+  if (user.author_level === "admin") {
+    res.redirect(`http://localhost:8081/administracao/${user.author_id}`);
+  } else if (user.author_level === "user") {
+    res.redirect(`http://localhost:8081/user/${user.author_id}`);
+  } else {
+    // para erros diferentes vai pra ca
+    res.redirect("http://localhost:8081");
+  }
+}
+
+module.exports = authenticateUser;
